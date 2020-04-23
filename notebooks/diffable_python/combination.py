5f01628a2b93a430efc414df0e3b5c22a865a5e5
Brian <brianmkcebm@gmail.com>
2020-04-23 13:11:47 +0100

    redo combination inhaler
    use different tables to identify combination tables

--- notebooks/diffable_python/combination.py
+++ notebooks/diffable_python/combination.py
@@ -22,27 +22,35 @@ import pandas as pd
 
 # +
 sql = '''
-WITH bnf_codes AS (  
-  SELECT DISTINCT bnf_code FROM measures.dmd_objs_with_form_route WHERE 
-  bnf_name LIKE '%/%/%'  ##I think all combination inhalers have / at least twice in their name
+WITH vmp_codes AS (  
+  SELECT DISTINCT id FROM dmd.vmp WHERE 
+  nm LIKE '%/%/%'  ##I think all combination inhalers have / at least twice in their name
   AND
   bnf_code LIKE '03%'
   AND
   (bnf_code NOT LIKE '0309%' AND # cough preps
    bnf_code NOT LIKE '0310%')    # systemic nasal decongestants
-  AND 
-  form_route NOT LIKE '%neb%'
   )
    
-SELECT "vmp" AS type, id, bnf_code, nm
-FROM dmd.vmp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
+SELECT "vmp" AS type, id, vmp.bnf_code, nm
+FROM dmd.vmp as vmp
+INNER JOIN
+measures.dmd_objs_with_form_route as dmd
+ON
+vmp.id = dmd.snomed_id
+WHERE id IN (SELECT * FROM vmp_codes)
+AND  form_route NOT LIKE '%neb%'
 
 UNION ALL
 
-SELECT "amp" AS type, id, bnf_code, descr
+SELECT "amp" AS type, id, amp.bnf_code, descr
 FROM dmd.amp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
+INNER JOIN
+measures.dmd_objs_with_form_route as dmd
+ON
+amp.id = dmd.snomed_id
+WHERE vmp IN (SELECT * FROM vmp_codes)
+AND  form_route NOT LIKE '%neb%'
 
 ORDER BY type, nm, bnf_code, id'''
 
@@ -50,3 +58,6 @@ resp_combination_products = bq.cached_read(sql, csv_path=os.path.join('..','data
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 resp_combination_products
+# -
+
+
