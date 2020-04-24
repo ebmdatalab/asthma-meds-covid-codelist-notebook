be8632960c57f0c36d188ceffd41cade80235002
Brian <brianmkcebm@gmail.com>
2020-04-24 15:21:57 +0100

    first commit of LTRA notebook

---
+++ notebooks/diffable_python/ltra.py
@@ -0,0 +1,47 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# Leukotriene receptor antagonist are a class of medicines used in asthma. The majority of prescribing in England is for montelukast. This notebook sets out the [NHS Dictionary of Medicines and Devices](https://ebmdatalab.net/what-is-the-dmd-the-nhs-dictionary-of-medicines-and-devices/) codes for LTRA
+
+from ebmdatalab import bq
+import os
+import pandas as pd
+
+# +
+##ltra
+sql = '''
+WITH bnf_codes AS (  
+  SELECT bnf_code FROM hscic.presentation WHERE  
+  bnf_code LIKE '030302%'  
+ 
+   )
+   
+SELECT "vmp" AS type, id, bnf_code, nm
+FROM dmd.vmp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+UNION ALL
+
+SELECT "amp" AS type, id, bnf_code, descr
+FROM dmd.amp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+ORDER BY type, bnf_code, id'''
+
+sabutamol_asthma = bq.cached_read(sql, csv_path=os.path.join('..','data','sabutamol_asthma.csv'))
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+sabutamol_asthma
