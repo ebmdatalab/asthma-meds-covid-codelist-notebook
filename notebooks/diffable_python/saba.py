93d7613fe239853d0814db0df4682ee0f849b9e9
Brian <brianmkcebm@gmail.com>
2020-04-26 11:54:59 +0100

    commit saba notebook

---
+++ notebooks/diffable_python/saba.py
@@ -0,0 +1,51 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+The following notebook contains NHS dictionary of medicines and devices codes
+
+#import libraries
+from ebmdatalab import bq
+import os
+import pandas as pd
+
+# +
+sql = '''
+WITH bnf_codes AS (  
+  SELECT DISTINCT bnf_code FROM measures.dmd_objs_with_form_route WHERE 
+  (bnf_code LIKE '0301011R0%' OR ##BNF salbutamol - excluded bnf_code LIKE '0301040R0%' as it only has comination with ipratropium in it
+   bnf_code LIKE '0301011V0%')    ##terbutaline
+  AND (form_route LIKE '%pressurizedinhalation.inhalation' OR form_route LIKE 'powderinhalation.inhalation%')
+   )
+   
+SELECT "vmp" AS type, id, bnf_code, nm
+FROM dmd.vmp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+UNION ALL
+
+SELECT "amp" AS type, id, bnf_code, descr
+FROM dmd.amp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+ORDER BY type, bnf_code, id'''
+
+saba_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','saba_codelist.csv'))
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+saba_codelist
+# -
+
+
